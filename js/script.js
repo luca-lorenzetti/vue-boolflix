824e03676f0826414ce0bea29ff854d6f555157b
Luca <luca.lorenzetti0@gmail.com>
2021-04-09 16:14:49 +0200

    Inserita prima base

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 let app = new Vue({
     el: '#root',
     data:{
+        input: '',
+        indexFilm: null,
+        filmsSearched: []
+    },
+    methods:{
 
+        serachByTitle(title){
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
+                    query: title
+                }
+              })
+              .then( (response) => {
+                    this.filmsSearched = response.data.results;
+                    console.log(this.filmsSearched)
+              });
+
+
+              
+            }
+    },
+
+    created(){
     }
 });
-
-
