c6d3c62ea9ed6b6a70c376feffbca94b457e2d20
Luca <luca.lorenzetti0@gmail.com>
2021-04-09 23:59:24 +0200

    Primo sistema parziale bandiere

--- js/script.js
+++ js/script.js
@@ -3,27 +3,73 @@ let app = new Vue({
     data:{
         input: '',
         indexFilm: null,
-        filmsSearched: []
+        indexStar: null,
+        filmsSearched: [],
+        movies: [],
+        tvs: []
+
     },
     methods:{
 
-        serachByTitle(title){
+        serachByTitle(title,movie,tv){
+
+            if( movie ){
+                this.getMovies(title);
+            }
+
+            if( tv ){
+                this.getTvs(title);
+            }
+            this.filmsSearched = [...this.movies, ...this.tvs];
+           
+        },
+
+        getMovies( title ){
+            
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
                     query: title
                 }
-              })
-              .then( (response) => {
-                    this.filmsSearched = response.data.results;
-                    console.log(this.filmsSearched)
-              });
+                })
+                .then( (response) => {
+                    this.movies = response.data.results;
 
+                    this.convertFlags(this.movies);
+           
+                });
+                
+        },
+        getTvs( title ){
 
-              
-            }
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                    api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
+                    query: title
+                }
+                })
+                .then( (response) => {
+                 this.tvs = response.data.results;
+                 this.convertFlags(this.tvs);
+
+                });
+        },
+        convertFlags(list){
+            list.forEach((item)=>{
+                switch(item.original_language){
+                    case "en":
+                        item.original_language = 'gb';
+                        break;
+                    case 'ja':
+                        item.original_language = 'jp';
+                        break;    
+                }
+            })
+
+        }  
     },
 
     created(){
+
     }
 });
